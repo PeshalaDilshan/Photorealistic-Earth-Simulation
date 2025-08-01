04372ad59d544937b457bbb3b05b93fd67474aeb
Peshala Dilshan <99715578+PeshalaDilshan@users.noreply.github.com>
2025-08-01 12:58:18 +0530

    Update textures.js

--- textures.js
+++ textures.js
@@ -10,3 +10,13 @@ const textureURLs = {
     moon: 'https://www.solarsystemscope.com/textures/download/2k_moon.jpg',
     stars: 'https://www.solarsystemscope.com/textures/download/2k_stars.jpg'
 };
+
+// Fallback textures (solid colors) if online textures fail
+const fallbackTextures = {
+    earthDay: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYGD4z8ABAALk/jG8AAAAAElFTkSuQmCC', // Blue
+    earthNight: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M/wHwAEBgI6C7k9AAAAAElFTkSuQmCC', // Dark gray
+    earthSpecular: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M/wHwAEBgI6C7k9AAAAAElFTkSuQmCC', // Dark gray
+    clouds: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8DwHwAFBQIAX8jx0gAAAABJRU5ErkJggg==', // White
+    moon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z/D/HwAFBQIAYeF0/gAAAABJRU5ErkJggg==', // Light gray
+    stars: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M/wHwAEBgI6C7k9AAAAAElFTkSuQmCC' // Dark gray
+};
