27410594c3dbe13d3fd3430a6670cd060fd31546
Peshala Dilshan <99715578+PeshalaDilshan@users.noreply.github.com>
2025-08-01 12:46:09 +0530

    Create textures.js

---
+++ textures.js
@@ -0,0 +1,12 @@
+// Textures from Solar System Scope (https://www.solarsystemscope.com/textures/)
+// Distributed under Attribution 4.0 International license
+// Please include attribution if you use these textures in your project.
+
+const textureURLs = {
+    earthDay: 'https://www.solarsystemscope.com/textures/download/2k_earth_daymap.jpg',
+    earthNight: 'https://www.solarsystemscope.com/textures/download/2k_earth_nightmap.jpg',
+    earthSpecular: 'https://www.solarsystemscope.com/textures/download/2k_earth_specular_map.jpg',
+    clouds: 'https://www.solarsystemscope.com/textures/download/2k_earth_clouds.jpg',
+    moon: 'https://www.solarsystemscope.com/textures/download/2k_moon.jpg',
+    stars: 'https://www.solarsystemscope.com/textures/download/2k_stars.jpg'
+};
